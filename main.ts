3543ab1bed8b94509732f188908b2fb262406825
Abdulrehman <you@example.com>
2024-06-15 10:38:17 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,18 @@
+//Creating Car variable
+
+let car: string = 'Subaru';
+//Test 1
+console.log("Is car === 'Subaru'? I predict True");
+console.log(car === 'Subaru');
+//Test 2
+console.log("Is car == 'Subaru'? I predict True");
+console.log(car == 'Subaru');
+// Test 3
+console.log("Is car != 'Honda'? I predict True");
+console.log(car != "Honda");
+//Test 4
+console.log("Is car !== 'Ford'? I predict True");
+console.log(car !== 'Ford');
+//Test 5
+console.log("Is car.Upper case !== 'SUBARU'? I predict True");
+console.log(car.toUpperCase() == 'SUBARU');
